38e57feafbff863286e439f38adbcdbf9e73eb39
JOSSIE_YAN <jiayin.yan.jossie@gmail.com>
2025-02-18 10:13:42 -0800

    Add step 6/7

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -17,7 +17,7 @@ var uiConfig = {
       },
       // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
       signInFlow: 'popup',
-      signInSuccessUrl: '<url-to-redirect-to-on-success>',
+      signInSuccessUrl: "main.html",
       signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
             // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -33,5 +33,4 @@ var uiConfig = {
       privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
-// The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
