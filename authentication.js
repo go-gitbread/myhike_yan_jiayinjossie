474ac58be1739263f8eef4a1087c9d8560ca2290
JOSSIE_YAN <jiayin.yan.jossie@gmail.com>
2025-02-18 09:54:46 -0800

    Update authentication

--- authentication.js
+++ authentication.js
@@ -17,7 +17,7 @@ var uiConfig = {
       },
       // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
       signInFlow: 'popup',
-      signInSuccessUrl: "main.html",
+      signInSuccessUrl: '<url-to-redirect-to-on-success>',
       signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
             // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -33,4 +33,5 @@ var uiConfig = {
       privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
+// The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
